2b91daa0ed8d5c4104c1608bf40ad52a35b2b6d0
jean-francoiss <jean.battu@alhambrait.com.br>
2021-10-01 08:11:12 -0300

    first commit

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+// DB Params
+define("DB_HOST", "localhost:8080");
+define("DB_USER", "root");
+define("DB_PASS", "root");
+define("DB_NAME", "joblister");
+
+define("SITE_TITLE", "JobLister");
+
