d17a92ed1199898e247fb81e509285714d19940e
jean.battu <dimerhacker@gmail.com>
2021-10-03 11:31:35 -0300

    First commit

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 // DB Params
-define("DB_HOST", "localhost:8080");
+define("DB_HOST", "localhost:3306");
 define("DB_USER", "root");
 define("DB_PASS", "root");
 define("DB_NAME", "joblister");
